f43dd771ad31c2bedf240a51c0d6bb552f8c7521
Gerry Ferdinandus <gerry_ferdinandus@hotmail.com>
2015-08-15 04:12:57 +0200

    Change version from (1.31) to (1.32 BETA)

--- source/main.pas
+++ source/main.pas
@@ -240,7 +240,7 @@ const
     //    'udp://open.demonii.com:1337/announce'
     );
   //program name and version
-  FORM_CAPTION = 'Bittorrent tracker editor (1.31)';
+  FORM_CAPTION = 'Bittorrent tracker editor (1.32 BETA)';
   TORRENT_FILES_CONTENTS_FORM_CAPTION =
     'Show all the files inside the torrents. (Use right mouse for popup menu.)';
 
