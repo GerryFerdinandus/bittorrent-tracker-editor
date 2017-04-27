e12ae22efb17968f84469c4b7163cf9826b56d5a
Gerry Ferdinandus <gerry_ferdinandus@hotmail.com>
2017-04-27 11:27:27 +0200

    Update version (1.32.0-RC.4) -> (1.32.0-RC.5)
    Update the software version for next release.

--- source/code/main.pas
+++ source/code/main.pas
@@ -265,7 +265,7 @@ const
     //    'udp://open.demonii.com:1337/announce'
     );
   //program name and version (http://semver.org/)
-  FORM_CAPTION = 'Bittorrent tracker editor (1.32.0-RC.4)';
+  FORM_CAPTION = 'Bittorrent tracker editor (1.32.0-RC.5)';
   TORRENT_FILES_CONTENTS_FORM_CAPTION =
     'Show all the files inside the torrents. (Use right mouse for popup menu.)';
 
