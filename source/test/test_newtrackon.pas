9a99c00637e392cda75e7aeb807ea174542ce501
Gerry Ferdinandus <gerry_ferdinandus@hotmail.com>
2017-09-25 13:15:06 +0200

    refactor: test_newtrackon.pas

--- source/test/test_newtrackon.pas
+++ source/test/test_newtrackon.pas
@@ -25,19 +25,25 @@ implementation
 
 procedure TTestNewTrackon.Test_DownloadAPI;
 begin
-  AssertTrue('Download the newtrackon API', FNewTrackon.DownloadTrackers);
+  Check(FNewTrackon.DownloadTrackers, 'Download the newtrackon API');
 
-  AssertTrue('TrackerList_All should never be empty',
-    FNewTrackon.TrackerList_All.Count > 0);
+  Check(FNewTrackon.TrackerList_All.Count > 0,
+    'TrackerList_All should never be empty');
 
-  AssertTrue('TrackerList_Live should never be empty',
-    FNewTrackon.TrackerList_Live.Count > 0);
+  Check(FNewTrackon.TrackerList_Live.Count > 0,
+    'TrackerList_Live should never be empty');
 
-  AssertTrue('TrackerList_Stable should never be empty',
-    FNewTrackon.TrackerList_Stable.Count > 0);
+  Check(FNewTrackon.TrackerList_Stable.Count > 0,
+    'TrackerList_Stable should never be empty');
 
-  AssertTrue('TrackerList_Dead should never be empty',
-    FNewTrackon.TrackerList_Dead.Count > 0);
+  Check(FNewTrackon.TrackerList_Udp.Count > 0,
+    'TrackerList_Udp should never be empty');
+
+  Check(FNewTrackon.TrackerList_Http.Count > 0,
+    'TrackerList_Http should never be empty');
+
+  Check(FNewTrackon.TrackerList_Dead.Count > 0,
+    'TrackerList_Dead should never be empty');
 end;
 
 procedure TTestNewTrackon.SetUp;
