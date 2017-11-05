8e58905f77677ac0d7aae46e0c323c73f7e90a82
Gerry Ferdinandus <gerry_ferdinandus@hotmail.com>
2017-11-05 05:36:00 +0100

    feature: add trackers upload API test
    There is an issue that the post routine keep waiting forever.
    Upload 3 URL. The test should always return with true.

--- source/test/test_newtrackon.pas
+++ source/test/test_newtrackon.pas
@@ -18,12 +18,13 @@ type
     procedure SetUp; override;
     procedure TearDown; override;
   published
-    procedure Test_DownloadAPI;
+    procedure Test_API_Download;
+    procedure Test_API_Upload;
   end;
 
 implementation
 
-procedure TTestNewTrackon.Test_DownloadAPI;
+procedure TTestNewTrackon.Test_API_Download;
 begin
   Check(FNewTrackon.DownloadEverything, 'Download the newtrackon API');
 
@@ -44,6 +45,29 @@ begin
 
   Check(FNewTrackon.TrackerList_Dead.Count > 0,
     'TrackerList_Dead should never be empty');
+end;
+
+procedure TTestNewTrackon.Test_API_Upload;
+var
+  TrackerList: TStringList;
+  TrackersSendCount: integer;
+begin
+  TrackerList := TStringList.Create;
+
+  //Add two trackers
+  TrackerList.Add('udp://tracker.leechers-paradise.org:6969/announce');
+  TrackerList.Add('udp://tracker.test.org:6969/announce');//dummy URL
+  TrackerList.Add('wss://tracker.openwebtorrent.com');
+
+  //Test if upload is OK
+  try
+    Check(FNewTrackon.SubmitTrackers(TrackerList, TrackersSendCount),
+      'Upload the newtrackon API');
+    Check(TrackersSendCount <= TrackerList.Count, 'TrackersSendCount have too high value');
+  finally
+    TrackerList.Free;
+  end;
+
 end;
 
 procedure TTestNewTrackon.SetUp;
